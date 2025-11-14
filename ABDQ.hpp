fd98bf38e5cb73c1aac6bb10158090ecb06c1fb9
Carter Braun <carterbraun79@gmail.com>
2025-11-13 22:49:01 -0500

    ABDQ adjustment

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -177,8 +177,14 @@ public:
     };
 
     // Access
-    const T& front() const override {return data_[front_];};
-    const T& back() const override {return data_[back_];};
+    const T& front() const override {
+        if (size_ == 0) throw std::runtime_error("");
+        return data_[front_];
+    };
+    const T& back() const override {
+        if (size_ == 0) throw std::runtime_error("");
+        return data_[back_];
+    };
 
     // Getters
     std::size_t getSize() const noexcept override {return size_;};
